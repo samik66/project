e2ed97a25d777d7a004a23463f67b0779dc1cfc2
sami66 <skim8@cps.edu>
2016-04-25 17:51:43 +0000

    first commit

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,18 @@
+class ProjectsController < ApplicationController
+    def home
+        render "home"
+    end
+    
+    def personal
+        render "personal"
+    end
+    
+    def sports
+        render "sports"
+    end
+    
+    def hobbies
+        render "hobbies"
+    end
+    
+end    
